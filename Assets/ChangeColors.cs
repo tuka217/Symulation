cb0cfe148b991fed778d0d5d412d1ee6ed8e27ed
tuka217 <a.walasek@gazeta.pl>
2015-12-01 00:27:40 +0100

    RGB - heat

--- Assets/ChangeColors.cs
+++ Assets/ChangeColors.cs
@@ -7,51 +7,120 @@ public class ChangeColors : MonoBehaviour {
     private float cachedY;
     private float minXValue;
     private float maxXValue;
-    private int currentHeat;
+    private float currentHeat;
+    private float quarterTmp;
     public int maxHeat;
     public Image visualHeat;
+    float G;
+    float B;
+    float R;
 	// Use this for initialization
+    float zmienna = 1.0f;
 	void Start () {
-        maxHeat = 840;
+        maxHeat = 100;
         cachedY = 0;
         maxXValue = 840;
         minXValue = 0;
-        currentHeat = maxHeat;
+        currentHeat = 0;
+        visualHeat.color = new Color(0, 0, 1.0f);
+        quarterTmp = maxHeat / 4;
+        R = 0;
+        G = 0;
+        B = 1.0f;
 	}
 	
-    private int CurrentHeat
+    private float CurrentHeat
     {
         get { return currentHeat; }
         set { 
                 currentHeat = value;
-                HandleHeat();
+               // HandleHeat();
             }
     }
 	// Update is called once per frame
 	void Update () {
-        currentHeat -= 1;
-        HandleHeat();
+        currentHeat += 0.01f;
+        //HandleHeat();
         if (currentHeat <= 0)
         {
             currentHeat = 0;
         }
+        changeHeatToRGBColor();
 	}
 
- 
-    private void HandleHeat()
+    private void changeHeatToRGBColor()
     {
-        float currentXValue = MapValues(currentHeat, 0, maxHeat, minXValue, maxXValue);
-        if (currentHeat > maxHeat / 2)
+        if(currentHeat <= quarterTmp){
+            visualHeat.color = upG(currentHeat);
+        }
+        else if(currentHeat > quarterTmp && currentHeat <= quarterTmp*2 ){
+            visualHeat.color = downB(currentHeat);
+        }
+        else if(currentHeat > quarterTmp*2 && currentHeat <= quarterTmp*3){
+            visualHeat.color = upR(currentHeat);
+        }
+        else if(currentHeat > quarterTmp*3 && currentHeat <= maxHeat){
+            visualHeat.color = downG(currentHeat);
+        }
+    }
+
+    private Color upG(float currentHeat)
+    {
+        float step = (currentHeat * 0.01f) / (this.quarterTmp / 100);
+        step = Mathf.Round(step * 100f) / 100f;
+        G = step;
+        G = Mathf.Round(G * 100f) / 100f;
+        return new Color(R,G,B);
+    }
+
+    private Color downB(float currentHeat)
+    {
+        float step =( (currentHeat * 0.01f) / (this.quarterTmp / 100) ) -1;
+        step = Mathf.Round(step * 100f) / 100f;
+        B = 1 - step;
+        B = Mathf.Round(B * 100f) / 100f;
+        return new Color(R, G, B);
+    }
+
+    private Color upR(float currentHeat)
+    {
+        float step = (currentHeat * 0.01f) / (this.quarterTmp / 100);
+        step = Mathf.Round(step * 100f) / 100f;
+        R = step -2;
+        R = Mathf.Round(R * 100f) / 100f;
+        return new Color(R, G, B);
+    }
+
+    private Color downG(float currentHeat)
+    {
+        float step =( (currentHeat * 0.01f) / (this.quarterTmp / 100)) ;
+        step = Mathf.Round(step * 100f) / 100f;
+        G =  1-(step -3);
+        G = Mathf.Round(G * 100f) / 100f;
+        Debug.Log("G " + G);
+        Debug.Log("current " + currentHeat);
+        return new Color(R, G, B);
+    }
+ 
+  /*  private void HandleHeat()
+    {
+       // float currentXValue = MapValues(currentHeat, 0, maxHeat, minXValue, maxXValue);
+        if (currentHeat > maxHeat)
         {
             visualHeat.color = new Color32((byte)MapValues(currentHeat, maxHeat / 2, maxHeat, 255, 0), 255, 0, 255);
+            
         }
         else //less than 50%
         {
             visualHeat.color = new Color32(255, (byte)MapValues(currentHeat, 0, maxHeat / 2, 0, 255), 0, 255);
+           
         }
     }
     private float MapValues(float x, float inMin, float inMax, float outMin, float outMax)
     {
         return (x - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
-    }
+
+       // float quarterMaxXValue = maxXValue / 4;
+        
+    }*/
 }
